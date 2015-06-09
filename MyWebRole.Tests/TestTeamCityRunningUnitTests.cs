175beb1a193ce4207b08cae61d57e928ebfcb846
Michael <vaiyanzi@gmail.com>
2015-06-09 13:09:56 +0800

    Add unit test for to test TeamCity Running test.

---
+++ MyWebRole.Tests/TestTeamCityRunningUnitTests.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace MyWebRole.Tests
+{
+    [TestFixture]
+    public class TestTeamCityRunningUnitTests
+    {
+        [Test]
+        public void Test()
+        {
+            //Arrange
+            var first = 1000;
+            var second = 2000;
+            
+            //Act
+            var sum = first + second;
+            
+            //Assert
+            sum.Should().Be(3000);
+        }
+    }
+}
