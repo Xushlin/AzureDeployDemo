14fd0d7905d330d4edbff67f51d8f2284ec39146
TomYangGang <yangg@shinetechchina.com>
2015-06-16 10:30:26 +0800

    add cache role

--- AzureCloudServiceDemo/MyWebRole/Controllers/HomeController.cs
+++ AzureCloudServiceDemo/MyWebRole/Controllers/HomeController.cs
@@ -3,14 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.ApplicationServer.Caching;
 
 namespace MyWebRole.Controllers
 {
     public class HomeController : Controller
     {
+        public static DataCacheFactory myFactory;
+        public static DataCache myCache;
+        public static Random randomizer = new Random();
+
         public ActionResult Index()
         {
             return View();
+        }
+
+        public string GetCacheMessage()
+        {
+            if (myCache == null)
+            {
+                myFactory = new DataCacheFactory();
+                myCache = myFactory.GetDefaultCache();
+                myCache.Put("MyCache", "Welcome to use cache!");
+            }
+
+            return  myCache.Get("MyCache") as string;
         }
 
         public ActionResult About()
